0bc79762968819a1a11d307872d77920e34fdba2
nicholas hung <nhung93@outlook.com>
2024-07-31 22:40:07 -0400

    minor styling edits + trying a custom component for the modal using web components! :D

--- modal.js
+++ modal.js
@@ -61,3 +61,87 @@ class Modal {
     });
   }
 }
+
+// helpful! https://github.com/mdn/web-components-examples/blob/main/popup-info-box-web-component/main.js
+class ModalComponent extends HTMLElement {
+  constructor(){
+    super();
+  }
+  
+  connectedCallback(){
+    console.log("added to page");
+    
+    // shadow root
+    const shadow = this.attachShadow({mode: "open"});
+    
+    // overlay div
+    const overlay = document.createElement("div");
+    overlay.setAttribute("class", "modal-overlay");
+    
+    // modal
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+    
+    // the area to display text
+    const textArea = document.createElement("div");
+    textArea.innerHTML = "<slot></slot>";
+    
+    // ok button
+    const okBtn = document.createElement("button");
+    okBtn.innerText = "ok";
+    okBtn.addEventListener("click", () => {
+      this.style.display = "none";
+    });
+    
+    // set the style for shadow dom
+    const style = document.createElement("style");
+    style.textContent = `
+      .modal {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1010;
+        text-align: center;
+        padding: 6px;
+        background-color: #fff;
+        width: auto;
+        height: auto;
+        border: 1px solid #ccc;
+        overflow-y: auto;
+      }
+    
+      .modal-overlay {
+        z-index: 1000;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        backgroundColor: #aaa;
+        opacity: 0.2;
+      }
+    `;
+    
+    // put it all together
+    shadow.append(style);
+    shadow.appendChild(overlay);
+    shadow.appendChild(modal);
+    modal.appendChild(textArea);
+    modal.appendChild(okBtn);
+  }
+  
+  disconnectedCallback(){
+    console.log("removed from page");
+  }
+  
+  adoptedCallback(){
+    console.log("moved to new page");
+  }
+  
+  attributeChangedCallback(name, oldVal, newVal){
+    console.log("attribute changed");
+  }
+}
+
+customElements.define("modal-component", ModalComponent);
